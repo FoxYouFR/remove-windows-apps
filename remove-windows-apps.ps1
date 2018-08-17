376d55431709fe64df3ab0b32727bedcdb0c5652
Florian Kolly <37935009+FoxYouFR@users.noreply.github.com>
2018-08-17 11:52:12 +0200

    Adding the V1.0 of the script

---
+++ remove-windows-apps.ps1
@@ -0,0 +1,30 @@
+﻿$array = Get-AppxPackage | Select Name, SignatureKind
+$number = 0
+
+foreach($element in $array){
+    $name = $element.Name
+    if($element.SignatureKind -eq "Store"){
+        $answer = Read-Host -Prompt "Do you want to remove $name ? (type yes to remove it)"
+        if($answer -eq "yes"){
+            echo "Trying to remove $name"
+            try{ 
+                $number++
+                Get-AppxPackage $element.Name | Remove-AppxPackage
+                echo "$name has been deleted"
+            } catch {
+                echo "An error occured, $name has not been uninstalled"
+                $numer--
+            }
+        }
+    }
+}
+
+if($number -gt 1){
+    echo "$number applications have been removed"
+} elseif ($number -eq 1){
+    echo "One application has been removed"
+} else {
+    echo "No application has been removed"
+}
+
+pause
